46be2d5a2f461063dd14d30ef5861926eeb8699e
Moitas500 <camilofour1@gmail.com>
2024-07-27 13:51:58 -0500

    Add validations

--- src/components/FormCreditCard.jsx
+++ src/components/FormCreditCard.jsx
@@ -2,7 +2,11 @@ import { Button, Form } from "react-bootstrap"
 import { useForm } from "react-hook-form"
 import { useDispatch, useSelector } from "react-redux"
 import { changeStatusTransaction } from "../redux/paymentSlice"
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { FaCcVisa, FaCcMastercard  } from "react-icons/fa"
 import environment from "../environments/environment"
+import { useState } from "react"
 
 const FormCreditCard = () => {
 
@@ -11,7 +15,34 @@ const FormCreditCard = () => {
     const payment = useSelector((state) => state.payment)
     const cartShopping = useSelector((state) => state.cart)
 
-    const { register, handleSubmit } = useForm()
+    const { register, formState: { errors } , handleSubmit } = useForm()
+
+    const [cardType, setCardType] = useState('')
+    const [cardNumber, setCardNumber] = useState('')
+
+    const formatCardNumber = (number) => {
+        return number.replace(/(\d{4})(?=\d)/g, '$1 ')
+    }
+
+    const handleCardNumberChange = (event) => {
+        const input = event.target.value.replace(/\s+/g, '')
+        const formattedNumber = formatCardNumber(input)
+        setCardNumber(formattedNumber)
+        setCardType(getCardType(input))
+    }
+
+    const getCardType = (number) => {
+        const visaPattern = /^4[0-9]{12}(?:[0-9]{3})?$/
+        const masterCardPattern = /^5[1-5][0-9]{14}$/
+
+        if (visaPattern.test(number)) {
+            return 'visa'
+        } else if (masterCardPattern.test(number)) {
+            return 'mastercard'
+        } else {
+            return ''
+        }
+    }
 
     const sha256 = async (chain) => {
         const encondedText = new TextEncoder().encode(chain);
@@ -26,7 +57,9 @@ const FormCreditCard = () => {
         return random
     }
 
-    const submit = (data) => {
+    const submit = (data) => { 
+        toast('El pago se esta realizando')
+
         dispatch(changeStatusTransaction(true))
 
         fetch(`${environment.apiUrlWompi}/tokens/cards`, {
@@ -35,7 +68,7 @@ const FormCreditCard = () => {
                 'Authorization': `Bearer ${environment.keyPublic}`
             },
             body: JSON.stringify({
-                number: data.number,
+                number: data.number.replace(/\s+/g, ''),
                 cvc: data.cvc,
                 exp_month: data.month,
                 exp_year: data.year,
@@ -78,6 +111,47 @@ const FormCreditCard = () => {
         })
     }
 
+    const validateErrors = () => {
+        const errorMessages = {
+            name: {
+                required: 'El nombre es requerido',
+                minLength: 'El nombre debe tener mínimo 5 caracteres'
+            },
+            email: {
+                required: 'El email es requerido',
+                pattern: 'No es un correo electronico'
+            },
+            number: {
+                required: 'El numero de tarjeta es requerido',
+                pattern: 'Solo se acepta VISA y MASTERCARD',
+            },
+            cvc: {
+                required: 'El codigo de seguridad es requerido',
+                minLength: 'El CSV tiene 3 digitos'
+            },
+            month: {
+                required: 'El mes es requerido',
+                minLength: 'Debe estar en el formato 00',
+                pattern: 'El mes debe estar desde el 1 al 12'
+            },
+            year: {
+                required: 'El año es requerido',
+                minLength: 'Debe estar en el formato 00'
+            },
+            installments: {
+                required: 'El número de cuotas es requerido',
+                maxLength: 'El numero de cuotas tiene que ser menor a 99'
+            }
+        };
+        
+        Object.keys(errorMessages).forEach((field) => {
+            const errorType = errors[field]?.type;
+            if (errorType && errorMessages[field][errorType]) {
+                toast.warning(errorMessages[field][errorType]);
+            }
+        });
+    }
+
     return (
         <Form onSubmit={handleSubmit(submit)} style={{
             display: "flex",
@@ -88,24 +162,38 @@ const FormCreditCard = () => {
                 width: "100%"
             }}>
                 <Form.Label>Nombre completo</Form.Label>
-                <Form.Control type="text" placeholder="Escriba el nombre completo" {...register('name', {required: true})}/>
+                <Form.Control type="text" placeholder="Escriba el nombre completo" {...register('name', {required: true, minLength: 5})}/>
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Email" {...register('email', {required: true})}/>
+                <Form.Control type="email" placeholder="Email" {...register('email', {required: true, pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/})}/>
                 <Form.Label>Numero</Form.Label>
-                <Form.Control type="text" placeholder="4000 0000 0000" {...register('number', {required: true})}/>
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    gap: '20px'
+                }}>
+                    {cardType === 'mastercard' && <FaCcMastercard />}
+                    {cardType === 'visa' && <FaCcVisa />}
+                    <Form.Control value={cardNumber} maxLength={19} type="text" placeholder="4000 0000 0000" {...register('number', {required: true, 
+                            pattern: /^(?:4[0-9]{3}\s?[0-9]{4}\s?[0-9]{4}\s?[0-9]{4}|5[1-5][0-9]{2}\s?[0-9]{4}\s?[0-9]{4}\s?[0-9]{4})$/,
+                        })}
+                        onChange={handleCardNumberChange}
+                    />
+                </div>
                 <Form.Label>CSV</Form.Label>
-                <Form.Control type="text" placeholder="000" {...register('cvc', {required: true})}/>
+                <Form.Control maxLength={3} type="text" placeholder="000" {...register('cvc', {required: true, minLength: 3})}/>
                 <Form.Label>Mes de fecha de expedicion</Form.Label>
-                <Form.Control type="text" placeholder="00" {...register('month', {required: true})}/>
+                <Form.Control maxLength={2} type="text" placeholder="00" {...register('month', {required: true, minLength: 2, pattern: /^(0[0-9]|1[0-2])$/})}/>
                 <Form.Label>Año de expedicion</Form.Label>
-                <Form.Control type="text" placeholder="00" {...register('year', {required: true})}/>
+                <Form.Control maxLength={2} type="text" placeholder="00" {...register('year', {required: true, minLength: 2, pattern: /^(0[0-9]|[1-9][0-9])$/})}/>
                 <Form.Label>Numero de cuotas</Form.Label>
-                <Form.Control type="text" placeholder="00" {...register('installments', {required: true})}/>
+                <Form.Control maxLength={2} type="text" placeholder="00" {...register('installments', {required: true, maxLength: 2})}/>
             </Form.Group>
 
-            <Button variant="primary" type="submit" disabled={payment.isTransactionMade}>
+            <Button variant="primary" type="submit" disabled={payment.isTransactionMade} onClick={validateErrors}>
                 Pay
             </Button>
+
+            <ToastContainer/>
         </Form>
     )
 
